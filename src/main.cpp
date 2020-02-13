9dc52c742410a809e988e9c71a557084620fd5a6
Sabaniki <sawada13@mccc.jp>
2020-02-14 00:43:49 +0900

    時間を更に微調整

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ void setup() {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-noreturn"
 void loop() {
-    unsigned long scheduledTime = Timer::createTime(0, 2, 11, 0);
+    unsigned long scheduledTime = Timer::createTime(0, 2, 10, 0);
     auto led = DigitalPin(13, OUTPUT);
     auto pin = DigitalPin(5, OUTPUT);
     pin.write(LOW);
